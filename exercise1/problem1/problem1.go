c263fed0a9ac3d67e8f9d2ba6985713c6882e689
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-06-13 15:37:08 +0500

    exercise1/problem1

--- exercise1/problem1/problem1.go
+++ exercise1/problem1/problem1.go
@@ -1,4 +1,16 @@
 package problem1
 
-func isChangeEnough() {
+func isChangeEnough(pocket [4]int, price float32) bool {
+	var total float32
+	coinValues := [4]float32{0.25, 0.10, 0.05, 0.01}
+
+	for index, coinValue := range coinValues {
+		total += coinValue * float32(pocket[index])
+
+		if total >= price {
+			return true
+		}
+	}
+
+	return false
 }
