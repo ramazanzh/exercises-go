2383827b9740a9f8518eb51ebec8d6508b71dee4
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-04 15:31:20 +0500

    exercise1/problem4

--- exercise1/problem4/problem4.go
+++ exercise1/problem4/problem4.go
@@ -1,4 +1,15 @@
 package problem4
 
-func mapping() {
+import (
+	"strings"
+)
+
+func mapping(letters []string) map[string]string {
+	res := make(map[string]string, len(letters))
+
+	for _, letter := range letters {
+		res[letter] = strings.ToUpper(letter)
+	}
+
+	return res
 }
