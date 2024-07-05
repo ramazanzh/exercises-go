32aeb3098488d493e96b1d6ca132d5026b652e66
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-05 16:10:36 +0500

    exercise1/problem5

--- exercise1/problem5/problem5.go
+++ exercise1/problem5/problem5.go
@@ -1,4 +1,29 @@
 package problem5
 
-func products() {
+import (
+	"sort"
+)
+
+//products({"Computer" : 600, "TV" : 800, "Radio" : 50}, 300) ➞ ["TV","Computer"]
+
+func products(entry map[string]int, minPrice int) []string {
+	keys := make([]string, 0, len(entry))
+
+	for key := range entry {
+		if minPrice > entry[key] {
+			continue
+		}
+
+		keys = append(keys, key)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		if entry[keys[i]] == entry[keys[j]] {
+			return keys[i] < keys[j]
+		}
+
+		return entry[keys[i]] > entry[keys[j]]
+	})
+
+	return keys
 }
