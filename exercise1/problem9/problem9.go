ceb87f6ffaed486b1bd5918bbe68c5065dfa8730
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-05 09:59:39 +0500

    exercise1/problem9

--- exercise1/problem9/problem9.go
+++ exercise1/problem9/problem9.go
@@ -1,3 +1,12 @@
 package problem9
 
-func factory() {}
+func factory(mult int) func(param ...int) []int {
+	return func(param ...int) []int {
+		var res []int
+		for _, value := range param {
+			res = append(res, mult*value)
+		}
+
+		return res
+	}
+}
