bcac39ab605d4bfde99be553a8cf5c2b3adbe97e
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-05 11:49:08 +0500

    exercise1/problem11

--- exercise1/problem11/problem11.go
+++ exercise1/problem11/problem11.go
@@ -1,3 +1,17 @@
 package problem11
 
-func removeDups() {}
+func removeDups[T comparable](input []T) []T {
+	var res []T
+	temp := map[T]bool{}
+
+	for _, value := range input {
+		if temp[value] {
+			continue
+		}
+
+		temp[value] = true
+		res = append(res, value)
+	}
+
+	return res
+}
