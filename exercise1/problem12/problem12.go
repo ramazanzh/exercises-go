f90a553283470d2f7bba3ed44bcff83bcd9730dc
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-05 15:36:02 +0500

    exercise1/problem12

--- exercise1/problem12/problem12.go
+++ exercise1/problem12/problem12.go
@@ -1,3 +1,24 @@
 package problem11
 
-func keysAndValues() {}
+import (
+	"sort"
+)
+
+func keysAndValues[T int | string, V any](input map[T]V) ([]T, []V) {
+	keys := make([]T, 0, len(input))
+	values := make([]V, 0, len(input))
+
+	for key, _ := range input {
+		keys = append(keys, key)
+	}
+
+	sort.Slice(keys, func(i, j int) bool {
+		return keys[i] < keys[j]
+	})
+
+	for _, value := range keys {
+		values = append(values, input[value])
+	}
+
+	return keys, values
+}
