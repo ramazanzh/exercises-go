a1e840e7a34dd1f6c6f7db060321f7cb5c912326
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-05 11:39:53 +0500

    exercise1/problem10

--- exercise1/problem10/problem10.go
+++ exercise1/problem10/problem10.go
@@ -1,3 +1,13 @@
 package problem10
 
-func factory() {}
+func factory() (map[string]int, func(name string) func(num int)) {
+	brands := map[string]int{}
+
+	return brands, func(name string) func(num int) {
+		brands[name] = 0
+
+		return func(num int) {
+			brands[name] += num
+		}
+	}
+}
