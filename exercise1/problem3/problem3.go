bcfbcdb58eaa7e8cfc93c3011746f66f21c0bfa9
Рамазан Жарылкасын <rzharylkassyn@jmart.kz>
2024-07-07 18:09:00 +0500

    exercise1/problem3

--- exercise1/problem3/problem3.go
+++ exercise1/problem3/problem3.go
@@ -9,5 +9,36 @@ const (
 	lr dir = "lr"
 )
 
-func diagonalize() {
+func diagonalize(n int, d dir) [][]int {
+	res := make([][]int, n)
+
+	for index, _ := range res {
+		res[index] = make([]int, n)
+
+		for i := 0; i < n; i++ {
+			res[index][i] = valueByIndex(d, n, index, i)
+		}
+	}
+
+	return res
+}
+
+func valueByIndex(d dir, n int, si int, i int) int {
+	if d == ul {
+		return si + i
+	}
+
+	if d == ur {
+		return n + si - 1 - i
+	}
+
+	if d == ll {
+		return n - 1 - si + i
+	}
+
+	if d == lr {
+		return (n * 2) - 2 - si - i
+	}
+
+	return 0
 }
